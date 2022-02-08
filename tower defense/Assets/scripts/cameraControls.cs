711885564ee50c6807bf523df6add857b77424a2
Vyachkoslav <dedplantoed@gmail.com>
2022-02-08 18:21:55 +0200

    on hover moving

--- tower defense/Assets/scripts/cameraControls.cs
+++ tower defense/Assets/scripts/cameraControls.cs
@@ -24,7 +24,7 @@ public class cameraControls : MonoBehaviour
     void Update()
     {
         float mouseX = Input.GetAxis("Mouse X");
-
+        // by buttons
         if (Input.GetKey("up"))
         {
             targetOfView.position += targetOfView.forward * cameraSpeed * Time.deltaTime;
@@ -42,17 +42,27 @@ public class cameraControls : MonoBehaviour
         {
             targetOfView.position += -right * cameraSpeed * Time.deltaTime;
         }
-
+        // rmb rotating
         if (Input.GetMouseButton(1))
         {
             eulerAngle += Vector3.up * sensitivity * mouseX;
             targetOfView.eulerAngles = eulerAngle;
         }
+        // reset
         if (Input.GetKeyDown("space"))
         {
             targetOfView.position = startPosition;
             eulerAngle = Vector3.zero;
             targetOfView.eulerAngles = eulerAngle;
+        }
+        // hover
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 screenCenter = new Vector3(Screen.width / 2, 0, Screen.height / 2);
+
+        if(mousePos.x < 5 || mousePos.x > Screen.width - 5 || mousePos.y < 5 || mousePos.y > Screen.height - 5)
+        {
+            Vector3 convertedMousePos = new Vector3(mousePos.x, 0, mousePos.y);
+            targetOfView.position += Quaternion.Euler(eulerAngle) * Vector3.Normalize(convertedMousePos - screenCenter) * cameraSpeed * Time.deltaTime;
         }
     }
 }
