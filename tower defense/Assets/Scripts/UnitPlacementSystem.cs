946d89e82a5432b13cb2b98e6220763b3fb6e8e9
HenriAnttila <henkkazor@gmail.com>
2022-05-19 20:04:23 +0300

    naming nitpick

--- tower defense/Assets/Scripts/UnitPlacementSystem.cs
+++ tower defense/Assets/Scripts/UnitPlacementSystem.cs
@@ -2,28 +2,28 @@ using UnityEngine;
 
 public class UnitPlacementSystem : MonoBehaviour
 {
-    UnitPlacer _placer;
+    UnitPlacer placer;
 
     void Awake()
     {
-        _placer = GetComponent<UnitPlacer>();
-        _placer.enabled = false;
+        placer = GetComponent<UnitPlacer>();
+        placer.enabled = false;
     }
 
     public void SetTarget(GameObject newTarget)
     {
-        GameObject selectionPreview = _placer.Selection.Preview;
+        GameObject selectionPreview = placer.Selection.Preview;
         if (selectionPreview && newTarget.name == $"{selectionPreview.name}(Clone)")
         {
             return;
         }
-        _placer.enabled = true;
-        _placer.SetTarget(newTarget);
+        placer.enabled = true;
+        placer.SetTarget(newTarget);
     }
 
     public void Disable()
     {
-        _placer.Selection.Clear();
-        _placer.enabled = false;
+        placer.Selection.Clear();
+        placer.enabled = false;
     }
 }
